8928f91ed3a4b0af77680dffb2e6f68f8ce3b8ee
Domen Tominc <letalec15@gmail.com>
2025-05-07 21:11:30 +0200

    Projekt verzija 1.4 upgrade

--- app/providers/AuthProvider.js
+++ app/providers/AuthProvider.js
@@ -1,6 +1,8 @@
 'use client';
-import { createClient } from '@/lib/supabase/client';
+
 import { useState } from "react";
+import { createClient } from '@/lib/supabase/client';
+import { SessionContextProvider } from '@supabase/auth-helpers-react'; // <- MANJKAJOČ UVOZ
 
 export default function AuthProvider({ children }) {
   const [supabaseClient] = useState(() => createClient());
