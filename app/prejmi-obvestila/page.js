f39c1266ab06a1f1097c99ba2dd7ac47f32d4966
Domen Tominc <letalec15@gmail.com>
2025-06-20 22:35:04 +0200

    Update projekta V5.1

--- app/prejmi-obvestila/page.js
+++ app/prejmi-obvestila/page.js
@@ -1,14 +1,7 @@
 // app/prejmi-obvestila/page.js
-import dynamic from 'next/dynamic'
+'use client';
 
-// 1) tell Next not to statically render this route at build time
-export const dynamic = 'force-dynamic'
-
-// 2) only load the browser‐only component on the client
-const PrejmiObvestilaClient = dynamic(
-  () => import('@/app/components/PrejmiObvestilaClient'),
-  { ssr: false }
-)
+import PrejmiObvestilaClient from '@/app/components/PrejmiObvestilaClient';
 
 export default function PrejmiObvestilaPage() {
   return <PrejmiObvestilaClient />;
